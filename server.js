bf959963bf87dbf603ad034779201418f759c0a5
Nate Boyette <nateboyette@icloud.com>
2019-06-16 20:58:28 -0700

    testing server.get

--- server.js
+++ server.js
@@ -11,13 +11,9 @@ const errorHandler = require("./middleware/errorHandler");
 const Users = require("./models/usersModel");
 
 server.get("/", async (req, res) => {
-  try {
-    const users = await Users.getAllUsers();
-    res.status(200).json({ message: "It's working", users });
-    // res.send(`It's working`);
-  } catch (error) {
-    res.status(500).json({ message: "A network error occurred" });
-  }
+  res.status(200).json({
+    api: "up"
+  });
 });
 
 server.use(errorHandler);
