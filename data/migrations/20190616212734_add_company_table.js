09368f5c096d2130d7f2ec84524223fb7398f378
Nate Boyette <nateboyette@icloud.com>
2019-06-16 22:06:09 -0700

    updated companies migration

--- data/migrations/20190616212734_add_company_table.js
+++ data/migrations/20190616212734_add_company_table.js
@@ -34,4 +34,6 @@ exports.up = function(knex, Promise) {
   });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("companies");
+};
